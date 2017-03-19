b52cc3862559d3abc764bd2ef68c954dafc76382
Kevin Chappell <kevin.b.chappell@gmail.com>
2017-03-19 23:24:18 +0000

    Fix isolate scope nonassign bug

--- src/ng-clamper.js
+++ src/ng-clamper.js
@@ -13,13 +13,15 @@ angular.module('ng-clamper', [])
   };
 })
 
-.controller('ClamperController', function ClamperController($scope, $element, $timeout, $window) {
-  'use strict';
+.controller('ClamperController', ['$scope', '$element', '$timeout', '$window', function ClamperController($scope, $element, $timeout, $window) {
 
   /*eslint angular/controller-as-vm: [2,"clamper"]*/
 
   var clamper = this,
-    scope = $scope;
+    scope = $scope,
+    curClamp,
+    clamped,
+    text;
 
   // strTruncate will shorten a string to a given length.
   // If substr cuts a word, it will find the last space and cut from there to respect word boundary.
@@ -35,26 +37,25 @@ angular.module('ng-clamper', [])
   }
 
   function setupClamper() {
-    scope.text = getText();
-    scope.ngModel.curClamp = scope.ngModel.clamp;
+    text = getText();
+    curClamp = scope.ngModel.clamp;
 
     // is it necessary to watch the clamp settings?
     scope.$watch('ngModel', function(newVal, oldVal){
       if (newVal.clamp !== oldVal.clamp) {
-        scope.ngModel.curClamp = newVal.clamp;
+        curClamp = newVal.clamp;
       }
       updateClamp();
     }, true);
 
     function updateClamp() {
-      var text = scope.text;
       var end = scope.ngModel.end || '&hellip;',
-        maxLines = parseInt(scope.ngModel.curClamp, 10),
+        maxLines = parseInt(curClamp, 10),
         lineHeight = $window.getComputedStyle($element[0], null).getPropertyValue('line-height').replace('px', ''),
         clampHeight = function() {
           return {
             current: $element[0].clientHeight || $element[0].offsetHeight,
-            closed: lineHeight * parseInt(scope.ngModel.curClamp, 10)
+            closed: lineHeight * parseInt(curClamp, 10)
           };
         },
         lines = function() {
@@ -75,13 +76,13 @@ angular.module('ng-clamper', [])
 
       $element[0].innerHTML = text;
 
-      if (!scope.ngModel.clamped) {
-        scope.ngModel.clamped = shouldClamp();
+      if (!clamped) {
+        clamped = shouldClamp();
       }
 
       trimLoop(text);
 
-      if (scope.ngModel.clamped) {
+      if (clamped) {
         $element.css({ 'max-height': '999px', 'overflow': 'auto' });
       } else {
         $element.css({ 'max-height': clampHeight().closed + 'px', 'overflow': 'hidden' });
@@ -93,15 +94,16 @@ angular.module('ng-clamper', [])
     });
 
     function doToggle() {
-      if (scope.ngModel.clamped) {
-        scope.ngModel.curClamp = 1000;
+      if (clamped) {
+        curClamp = 1000;
       } else {
-        scope.ngModel.curClamp = scope.ngModel.clamp;
+        curClamp = scope.ngModel.clamp;
       }
 
-      scope.ngModel.clamped = !scope.ngModel.clamped;
+      clamped = !clamped;
+      updateClamp();
     }
   }
 
   $timeout(setupClamper, 0);
-});
+}]);
